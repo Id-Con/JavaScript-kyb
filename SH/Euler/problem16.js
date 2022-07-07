5b25c6fa02fef154efc0806a2fcc86f4aaf64765
JwaSangHeon <whk4051@naver.com>
2022-07-07 18:04:46 +0900

    오일러 16번

--- SH/Euler/problem16.js
+++ SH/Euler/problem16.js
@@ -1,14 +1,28 @@
-const multi = 100;
+const multi = 1000;
 
-const num = 2 ** multi;
-let sum = [];
+const num = BigInt(2 ** multi);
 
-// console.log(num.toString().length);
+const num_string = num.toLocaleString("fullwide", { useGrouping: false });
 
-for (let i = 0; i < num.toString().length; i++) {
-  // sum.push(Number(num.toString()[i]));
-  console.log(Number(num.toString()[i]));
+console.log(num);
+console.log(num_string);
+
+let sum = 0;
+console.log(num_string[0]);
+for (let index = 0; index < num_string.length; index++) {
+  sum += parseInt(num_string[index]);
 }
 
-const result = sum.reduce((pre, cur) => pre + cur, 0);
-console.log(result);
+console.log(sum);
+
+// let sum = [];
+
+// // console.log(num.toString().length);
+
+// for (let i = 0; i < num.toString().length; i++) {
+//   // sum.push(Number(num.toString()[i]));
+//   console.log(Number(num.toString()[i]));
+// }
+
+// const result = sum.reduce((pre, cur) => pre + cur, 0);
+// console.log(result);
