f54edbc763d6b40807739b4cddf6df5164ab2b7f
kimkyeongbeom <bibix321@naver.com>
2022-07-07 16:45:23 +0900

    22.7.7_1

--- KB/hello.js
+++ KB/hello.js
@@ -1,1 +1,10 @@
-console.log(`Hello world!`);
+let obj = new Map();
+obj.set(1,11);
+obj.set(2,22);
+console.log(obj);
+console.log(obj.size);
+console.log(obj.has(11));
+console.log(...obj);
+for(let [key,value] of obj){
+    console.log(key, value);
+}
