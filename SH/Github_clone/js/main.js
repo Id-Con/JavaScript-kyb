acbfc73d28a2b8d6d099ce69d20df598744158bc
JwaSangHeon <whk4051@naver.com>
2022-07-15 16:40:07 +0900

    code-section 구현

--- SH/Github_clone/js/main.js
+++ SH/Github_clone/js/main.js
@@ -1,18 +1,26 @@
-const saTriggerMargin = 300;
-const saElementList = document.querySelectorAll(".sa");
+// const saTriggerMargin = 300;
+// const saElementList = document.querySelectorAll(".sa");
 
-const saFunc = function () {
-  for (const element of saElementList) {
-    if (!element.classList.contains("show")) {
-      if (
-        window.innerHeight >
-        element.getBoundingClientRect().top + saTriggerMargin
-      ) {
-        element.classList.add("show");
-      }
-    }
-  }
-};
+// const saFunc = function () {
+//   for (const element of saElementList) {
+//     if (!element.classList.contains("show")) {
+//       if (
+//         window.innerHeight >
+//         element.getBoundingClientRect().top + saTriggerMargin
+//       ) {
+//         element.classList.add("show");
+//       }
+//     }
+//     if (element.classList.contains("show")) {
+//       if (window.scrollY > element.getBoundingClientRect().bottom)
+//         element.classList.remove("show");
+//     }
+//   }
+// };
 
-window.addEventListener("load", saFunc);
-window.addEventListener("scroll", saFunc);
+// window.addEventListener("load", saFunc);
+// window.addEventListener("scroll", saFunc);
+
+AOS.init({
+  duration: 1000,
+}); // 자바스크립트로 init()을 해야 동작한다.
